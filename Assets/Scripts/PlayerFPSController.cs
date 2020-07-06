6907c74a35d7f628674fff3e01369a8d80159baa
alfianAH <alfian.hamdani04@gmail.com>
2020-07-06 23:23:10 +0800

    Add Item Scriptable Object

--- Assets/Scripts/PlayerFPSController.cs
+++ Assets/Scripts/PlayerFPSController.cs
@@ -6,7 +6,7 @@ public class PlayerFPSController : MonoBehaviour
     public FixedJoystick moveJoystick;
     public FixedButton jumpButton;
     public FixedTouchField touchField;
-
+    
     private RigidbodyFirstPersonController fps;
 
     // Start is called before the first frame update
@@ -18,6 +18,7 @@ public class PlayerFPSController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move Player
         fps.RunAxis = moveJoystick.Direction;
         fps.JumpAxis = jumpButton.pressed;
         fps.mouseLook.LookAxis = touchField.touchDist;
