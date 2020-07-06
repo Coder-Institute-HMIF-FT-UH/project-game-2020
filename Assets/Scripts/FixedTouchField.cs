b4411a9c34bb7b0f1ca154fb5ce5f910cfa18321
alfianAH <alfian.hamdani04@gmail.com>
2020-07-06 19:50:08 +0800

    Make player movement, rotation, and jump

---
+++ Assets/Scripts/FixedTouchField.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [HideInInspector]
+    public Vector2 touchDist;
+    [HideInInspector]
+    public Vector2 pointerOld;
+    
+    [HideInInspector]
+    public bool pressed;
+    protected int PointerId;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            if (PointerId >= 0 && PointerId < Input.touches.Length)
+            {
+                touchDist = Input.touches[PointerId].position - pointerOld;
+                pointerOld = Input.touches[PointerId].position;
+            }
+            else
+            {
+                touchDist = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - pointerOld;
+                pointerOld = Input.mousePosition;
+            }
+        }
+        else
+        {
+            touchDist = new Vector2();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+        PointerId = eventData.pointerId;
+        pointerOld = eventData.position;
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+    
+}
---
+++ Assets/Scripts/PlayerFPSController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerFPSController : MonoBehaviour
+{
+    public FixedJoystick moveJoystick;
+    public FixedButton jumpButton;
+    public FixedTouchField touchField;
+
+    private RigidbodyFirstPersonController fps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fps = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fps.RunAxis = moveJoystick.Direction;
+        fps.JumpAxis = jumpButton.pressed;
+        fps.mouseLook.LookAxis = touchField.touchDist;
+    }
+}
