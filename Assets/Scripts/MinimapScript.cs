53b9c532c7d8ee866cd9de27d487a3dfb2d22ba6
alfianAH <alfian.hamdani04@gmail.com>
2020-07-13 22:35:20 +0800

    Make MiniMap and HomeScene

---
+++ Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapScript : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Image gps;
+
+    private void LateUpdate()
+    {
+        // Minimap follow Player's position
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+        
+        // Minimap follow Player's rotation.y
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+
+    /// <summary>
+    /// GpsOn: Activate GPS
+    /// </summary>
+    public void GpsOn()
+    {
+        gps.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// GpsOff: Deactivate GPS
+    /// </summary>
+    public void GpsOff()
+    {
+        gps.gameObject.SetActive(false);
+    }
+}
