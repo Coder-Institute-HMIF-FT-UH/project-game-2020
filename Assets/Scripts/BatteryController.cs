968422073a0052b97af1c8647cf1a191501192b7
alfianAH <alfian.hamdani04@gmail.com>
2020-07-14 15:12:34 +0800

    Make GPS with battery

---
+++ Assets/Scripts/BatteryController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryController : MonoBehaviour
+{
+    public Slider batteryLevel;
+
+    [Space(10)] public Image batteryFill;
+
+    public Color currentBatteryColor,
+        highBatteryColor,
+        mediumBatteryColor,
+        lowBatteryColor;
+
+    private void LateUpdate()
+    {
+        if (batteryLevel.value < 0.3f)
+        {
+            currentBatteryColor = Color.Lerp(batteryFill.color, lowBatteryColor, Time.deltaTime);
+        }
+        else if (batteryLevel.value < 0.6f)
+        {
+            currentBatteryColor = Color.Lerp(batteryFill.color, mediumBatteryColor, Time.deltaTime);
+        }
+        else
+        {
+            currentBatteryColor = Color.Lerp(batteryFill.color, highBatteryColor, Time.deltaTime);
+        }
+
+        batteryFill.color = currentBatteryColor;
+    }
+}
