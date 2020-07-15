f0b8e3c5332502fa9e5fb9c742aa94cd7d3d7615
alfianAH <alfian.hamdani04@gmail.com>
2020-07-15 20:39:49 +0800

    Change battery drop speed to serializable

--- Assets/Scripts/BatteryController.cs
+++ Assets/Scripts/BatteryController.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class BatteryController : MonoBehaviour
 {
-    private float speed = 0.007f;
+    [SerializeField] private float speed = 0.007f;
     
     public Slider batteryLevel;
 
