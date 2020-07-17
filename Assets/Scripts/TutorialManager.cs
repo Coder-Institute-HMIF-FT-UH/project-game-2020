89fd4056b731b0e32163531deec653a2c57951ac
alfianAH <alfian.hamdani04@gmail.com>
2020-07-17 23:11:28 +0800

    Add TutorialManager

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private BoxCollider fakeDoor;
+    
+    // Controller
+    [Header("Controller")] 
+    [SerializeField] private GameObject jumpButton;
+    [SerializeField] private GameObject gpsButton;
+    
+    // Tutorial Dialogue
+    [Header("Tutorial Dialogue")]
+    [SerializeField] private GameObject[] tutorials;
+    
+    
+    private int tutorialIndex;
+
+    private void Start()
+    {
+        jumpButton.SetActive(false);
+        gpsButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < tutorials.Length; i++)
+        {
+            if(i == tutorialIndex)
+                tutorials[i].SetActive(true);
+            else
+                tutorials[i].SetActive(false);
+        }
+        
+        // Check tutorialsIndex
+    }
+}
