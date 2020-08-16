25ba0c131f4191a196cc21025d5320d92a9766d8
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-16 17:17:22 +0800

    Make level details scriptable object

---
+++ Assets/Scripts/LevelDetails.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "NewLevel", menuName = "Level")]
+public class LevelDetails : ScriptableObject
+{
+    // Details
+    public string levelName;
+    public Text levelText;
+    public string status;
+    public Text statusText;
+    
+    // Requirements
+    public int sanityRequirement;
+    
+    // Rewards
+    public int maxCoin;
+    public int starsTaken;
+    
+    // Best time
+    public int bestSeconds;
+    public int bestMinutes;
+    public int bestHours;
+}
