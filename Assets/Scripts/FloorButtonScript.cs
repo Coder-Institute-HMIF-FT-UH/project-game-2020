dc54d0caa4c619624a9022d647b40ff4ab2b04c5
Abdul-Khalik <56622781+Abdul-Khalik@users.noreply.github.com>
2020-08-21 00:02:12 +0800

    Added ButtonDoor Scene

---
+++ Assets/Scripts/FloorButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FloorButtonScript : MonoBehaviour
+{
+    [SerializeField] private GameObject linkedDoor; //Put the doors that want to get linked with the button here 
+
+    private Animator doorAnimator;
+
+    private void Start()
+    {
+        doorAnimator = linkedDoor.GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Item") || other.CompareTag("Player"))
+        {
+            doorAnimator.SetBool("IsClosed", false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorAnimator.SetBool("IsClosed", true);
+    }
+}
