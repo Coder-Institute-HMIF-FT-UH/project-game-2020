cc422f802ef2b10a3553b614569d130a5c71fa19
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-21 22:35:20 +0800

    Make scriptable object for item in shop

---
+++ Assets/Scripts/Shop/ItemUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour
+{
+    public ShopScriptableObject shopScriptableObject;
+    [SerializeField] private Image itemSprite;
+    [SerializeField] private Text priceValue;
+
+    private void Awake()
+    {
+        itemSprite.sprite = shopScriptableObject.itemSprite;
+        // Format int to price-like number
+        priceValue.text = $"{shopScriptableObject.price:N0}";
+    }
+}
---
+++ Assets/Scripts/Shop/ShopScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Shop")]
+public class ShopScriptableObject : ScriptableObject
+{
+    public string itemName;
+    public int price;
+    public Sprite itemSprite;
+}
