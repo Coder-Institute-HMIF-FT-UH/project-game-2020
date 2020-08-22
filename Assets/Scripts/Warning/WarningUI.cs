b85079dc55ddc9449c4defaacfbd02b9f84e0b1a
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-22 22:26:26 +0800

    Make warning scriptable objects

--- Assets/Scripts/Warning/WarningUI.cs
+++ Assets/Scripts/Warning/WarningUI.cs
@@ -12,7 +12,7 @@ public class WarningUI : MonoBehaviour
         if (warningScriptableObject)
         {
             itemHolder.sprite = warningScriptableObject.itemHolder;
-            lowItemText.text = warningScriptableObject.lowItemText;
+            lowItemText.text = $"LOW {warningScriptableObject.lowItemText}";
         }
     }
 }
