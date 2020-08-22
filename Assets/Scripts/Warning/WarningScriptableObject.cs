22b5a6a3bd046677a17e1fb73c54192d50f5c98c
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-22 22:00:12 +0800

    Make warning User Interface

---
+++ Assets/Scripts/Warning/WarningScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewWarning", menuName = "Warning")]
+public class WarningScriptableObject : ScriptableObject
+{
+    public string lowItemText;
+    public Sprite itemHolder;
+}
---
+++ Assets/Scripts/Warning/WarningUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WarningUI : MonoBehaviour
+{
+    public WarningScriptableObject warningScriptableObject;
+    [SerializeField] private Image itemHolder;
+    [SerializeField] private Text lowItemText;
+
+    public void SetWarningUI()
+    {
+        if (warningScriptableObject)
+        {
+            itemHolder.sprite = warningScriptableObject.itemHolder;
+            lowItemText.text = warningScriptableObject.lowItemText;
+        }
+    }
+}
