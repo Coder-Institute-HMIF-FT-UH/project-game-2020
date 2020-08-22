94fffa9cbbe2c4d9dd3e84c3519a64875d0c736a
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-22 23:09:58 +0800

    Get coin prefs in HomeScene

---
+++ Assets/Scripts/Coin/CoinManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    [SerializeField] private Text coinValue;
+    
+    private void Awake()
+    {
+        long currentCoin = Convert.ToInt64(PlayerPrefs.GetString(PlayerPrefsConstant.Coins));
+        coinValue.text = $"{currentCoin:N0}";
+    }
+}
