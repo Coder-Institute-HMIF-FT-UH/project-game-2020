2440bf4d5a9ddcc93f29202635f2e28fc105c656
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-25 20:40:18 +0800

    Tidy up the code

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -5,44 +5,36 @@ public class MovingPlatformScript : MonoBehaviour
     [SerializeField] private Vector3 travelDistance;
     [SerializeField] private float platformSpeed;
 
-    Vector3[] platformPosition = new Vector3[2];    // [0] : starting position
-                                                    // [1] : end position
+    private Vector3[] platformPosition = new Vector3[2];    // [0] : starting position
+                                                            // [1] : end position
     private int currentDestination = 1;
     
-    void Start()
+    private void Start()
     {
-        //Puts the platform on the startingp osition
+        // Puts the platform on the starting  position
         platformPosition[0] = transform.position;
         platformPosition[1] = transform.position + travelDistance;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-
         if(transform.position == platformPosition[currentDestination])
         {
-            if(currentDestination == 1)
-            {
-                currentDestination = 0;
-            }
-            else
-            {
-                currentDestination = 1;
-            }
+            currentDestination = currentDestination == 1 ? 0 : 1;
         }
-        //Moves the platform to current destination
+        // Moves the platform to current destination
         transform.position = Vector3.MoveTowards(transform.position, platformPosition[currentDestination], Time.fixedDeltaTime * platformSpeed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Used to attach other game object to this platform
+        // Used to attach other game object to this platform
         other.transform.parent = transform;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //Used to detach other game object to this platform
+        // Used to detach other game object to this platform
         other.transform.parent = null;
     }
 }
