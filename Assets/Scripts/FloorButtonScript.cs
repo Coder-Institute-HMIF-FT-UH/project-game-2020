14768f8379148baeddf07e9493fdf364fba97a6b
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-25 22:14:30 +0800

    Fix bugs and tidy up the code

--- Assets/Scripts/FloorButtonScript.cs
+++ Assets/Scripts/FloorButtonScript.cs
@@ -2,7 +2,8 @@
 
 public class FloorButtonScript : MonoBehaviour
 {
-    [SerializeField] private GameObject linkedDoor; //Put the doors that want to get linked with the button here 
+    // Put the doors that want to get linked with the button here 
+    [SerializeField] private GameObject linkedDoor; 
 
     private Transform doorTransform;
     private Collider doorCollider;
@@ -13,25 +14,24 @@ public class FloorButtonScript : MonoBehaviour
     private void Start()
     {
         doorTransform = linkedDoor.GetComponent<Transform>();
+        doorCollider = linkedDoor.GetComponent<Collider>();
         doorCloseState = doorTransform.transform.position;
         doorOpenState = doorTransform.transform.position + new Vector3(0, 3);
-
-        doorCollider = linkedDoor.GetComponent<Collider>();
     }
 
     private void Update()
     {
-        if (!buttonPressed) //Door is closed when there are no colliders on the button
+        // Door is closed when there are no colliders on the button
+        if (buttonPressed) return;
+        if (distanceTraveled > 0)
         {
-            if (distanceTraveled > 0)
-            {
-                doorTransform.transform.position = Vector3.LerpUnclamped(doorCloseState, doorOpenState, distanceTraveled); //Moves the door down
-                distanceTraveled -= 0.02f;
-            }
-            else
-            {
-                doorCollider.enabled = true;
-            }
+            // Moves the door down
+            doorTransform.transform.position = Vector3.LerpUnclamped(doorCloseState, doorOpenState, distanceTraveled);
+            distanceTraveled -= 0.02f;
+        }
+        else
+        {
+            doorCollider.enabled = true;
         }
     }
 
@@ -42,19 +42,21 @@ public class FloorButtonScript : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player") || other.CompareTag("Item"))//Door is opened when the collider is a game object with a player/item tag
+        // Check if other is player or Item
+        if (!other.CompareTag("Player") && !other.CompareTag("Item")) return;
+        buttonPressed = true;
+        if(distanceTraveled < 2)
         {
-            buttonPressed = true;
-            if(distanceTraveled < 1)
-            {
-                doorTransform.transform.position = Vector3.LerpUnclamped(doorCloseState, doorOpenState, distanceTraveled); //Moves the door up
-                distanceTraveled += 0.02f;
-            }
+            // Moves the door up
+            doorTransform.transform.position = Vector3.LerpUnclamped(doorCloseState, doorOpenState, distanceTraveled);
+            distanceTraveled += 0.02f;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.CompareTag("Player") || other.CompareTag("Item")) return;
+        Debug.Log("tidak ada apa");
         buttonPressed = false;
     }
 }
