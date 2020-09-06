ec09153700352672babbd72fb2569193d63f0ccb
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-06 21:51:07 +0800

    Make object spawner if object comes off the wall

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform upperSpawner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            Debug.Log("Spawn");
+            other.transform.position = upperSpawner.position;
+        }
+    }
+}
