2c61ff164f3b4e974396eea7bdc5e2c77dc3e647
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-25 23:07:54 +0800

    Set star sprites in select level

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -6,10 +6,13 @@ public class LevelManager : MonoBehaviour
     public LevelDetails levelDetails;
     [SerializeField] private Text levelName;
     [SerializeField] private DetailsManager detailsManager;
-
+    [SerializeField] private Sprite[] stars;
+    [SerializeField] private Image starSides;
+    
     private void Awake()
     {
         levelName.text = levelDetails.levelName;
+        SetStarSides();
     }
 
     public void SetDetails()
@@ -17,5 +20,12 @@ public class LevelManager : MonoBehaviour
         detailsManager.levelDetails = levelDetails;
         detailsManager.SeeDetails = true;
         detailsManager.IsDetailsTouch = true;
+    }
+
+    private void SetStarSides()
+    {
+        int starsTaken = PlayerPrefs.GetInt(PlayerPrefsConstant.StarsTaken + levelDetails.levelName);
+        
+        starSides.sprite = stars[starsTaken];
     }
 }
