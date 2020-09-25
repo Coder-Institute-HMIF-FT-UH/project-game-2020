2693b7720406502e05bb06248ab0e993e80c2df4
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-25 22:52:15 +0800

    Add how to exit properly

---
+++ Assets/Scripts/MobileButtonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MobileButtonController : MonoBehaviour
+{
+    private enum BackTo { Home, Exit }
+    [SerializeField] private SceneLoader sceneLoader;
+    [SerializeField] private GameObject exitContainer;
+    [SerializeField] private BackTo backTo;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            switch (backTo)
+            {
+                case BackTo.Home:
+                    sceneLoader.gameObject.SetActive(true);
+                    sceneLoader.LoadScene("HomeScene");
+                    break;
+                case BackTo.Exit:
+                    exitContainer.SetActive(true);
+                    break;
+            }
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
